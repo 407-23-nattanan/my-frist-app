0757aec5f199349cfc094275804073e41663e0d4
407-23-nattanan <54544@yupparaj.ac.th>
2026-09-11 11:27:41 +0700

    Update GelloBread.py

--- GelloBread.py
+++ GelloBread.py
@@ -4,4 +4,4 @@ st.title("GelloBread🥐")
 st.divider() 
 
 st.header("Order Summary")
-A = st.number_input("ครัวซองค์  45บาท/piece", value=0, step=1)
+A = st.number_input("ครัวซองค์  45บาท/piece",min_value=0, value=0, step=1)
